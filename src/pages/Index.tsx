ed039423453b2a85861668c4c58ac3cfec90bd29
RajeshGajengi <gajengi95@gmail.com>
2026-04-25 12:56:04 +0530

    testimonials added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
+import Testimonials from "@/components/Testimonials";
 import PortfolioSection from "@/components/PortfolioSection";
 import ContactSection from "@/components/ContactSection";
 // import Footer from "@/components/Footer";
@@ -14,6 +15,7 @@ const Index = () => {
       <AboutSection />
       <ServicesSection />
       <PortfolioSection />
+      <Testimonials />
       <ContactSection />
       {/* <Footer /> */}
     </div>
