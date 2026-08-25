6c52aac0d949e9863822b7a84da90c8d8a1bbcb5
RajeshGajengi <gajengi95@gmail.com>
2026-08-25 21:09:41 +0530

    error was getting at service page  because i didnt added this

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async"; // 1. Added the missing import
 
 // Global Components
 import Navbar from "./components/Navbar";
@@ -43,6 +44,8 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      {/* 2. Wrapped everything inside HelmetProvider right here */}
+      <HelmetProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
@@ -98,6 +101,7 @@ const App = () => (
         <Footer />
         </div>
       </BrowserRouter>
+      </HelmetProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
